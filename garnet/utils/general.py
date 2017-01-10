7a9e6889af2e1850c6f5625fb332670a7ef835fc
Peter Harrison <peter@colovore.com>
2017-01-10 06:32:17 -0800

    Fixed posting data to be UTC

--- garnet/utils/general.py
+++ garnet/utils/general.py
@@ -4,10 +4,11 @@
 import os
 import shutil
 import json
-import time
 import subprocess
 import locale
 import hashlib
+from datetime import datetime
+
 # Pip libraries
 import yaml
 
@@ -161,7 +162,7 @@ def normalized_timestamp(timestamp=None):
 
     # Process data
     if timestamp is None:
-        value = (int(time.time()) // interval) * interval
+        value = (int(datetime.utcnow().timestamp()) // interval) * interval
     else:
         value = (int(timestamp) // interval) * interval
     # Return
